623ef09707b985c169e72e9d6a6700057847f9a0
Nitin Kumar Pal <nitinpal900@gmail.com>
2024-03-15 00:06:27 +0530

    creating upcoming webinars section

---
+++ musicnextjs/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import TestimonialCards from "@/components/TestimonialCards";
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection/>
+      <FeaturedCourses/>
+      <WhyChooseUs/>
+      <TestimonialCards/>
+    </main>
+  );
+}

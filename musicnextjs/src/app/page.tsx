0c5b3c9cfc5baf374139aad47b163d8113800b68
Nitin Kumar Pal <nitinpal900@gmail.com>
2024-03-19 22:38:08 +0530

    DONE

--- musicnextjs/src/app/page.tsx
+++ musicnextjs/src/app/page.tsx
@@ -3,6 +3,9 @@ import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -10,6 +13,9 @@ export default function Home() {
       <FeaturedCourses/>
       <WhyChooseUs/>
       <TestimonialCards/>
+      <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
     </main>
   );
 }

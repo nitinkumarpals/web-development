7f39aab178871209ba1daa87231d5b0992362403
Nitin Kumar Pal <nitinpal900@gmail.com>
2024-03-20 23:13:27 +0530

    started adding components

---
+++ nike-rosy/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Hero,PopularProducts,SuperQuality,Services,SpecialOffers,CustomerReviews,Footer,Subscribe } from './sections/index.js'
+import Nav from './components/Nav.jsx';
+export default function App() {
+  return (
+    <main className="relative">
+      <Nav/>
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero/>
+      </section>
+      <section className="padding">
+        <PopularProducts/>
+      </section>
+      <section className="padding">
+        <SuperQuality/>
+      </section>
+      <section className="padding-x py-10">
+        <Services/>
+      </section>
+      <section className="padding">
+        <SpecialOffers/>
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews/>
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe/>
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer/>
+      </section>
+    </main>
+  )
+}
